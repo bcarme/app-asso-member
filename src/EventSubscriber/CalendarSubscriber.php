add13aaa89347ab88278d129a2bfb999705788ef
bcarme <barbara_carme@yahoo.fr>
2020-07-16 15:10:43 +0200

    calendar subscriber finally working and style done

--- src/EventSubscriber/CalendarSubscriber.php
+++ src/EventSubscriber/CalendarSubscriber.php
@@ -2,13 +2,29 @@
 
 namespace App\EventSubscriber;
 
+use App\Repository\BookingRepository;
 use CalendarBundle\CalendarEvents;
 use CalendarBundle\Entity\Event;
 use CalendarBundle\Event\CalendarEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class CalendarSubscriber implements EventSubscriberInterface
 {
+    // private $bookingRepository;
+    private $router;
+    private $bookingRepository;
+
+    public function __construct(
+        // BookingRepository $bookingRepository,
+        BookingRepository $bookingRepository,
+        UrlGeneratorInterface $router
+    ) {
+        // $this->bookingRepository = $bookingRepository;
+        $this->router = $router;
+        $this->bookingRepository = $bookingRepository;
+    }
+
     public static function getSubscribedEvents()
     {
         return [
@@ -22,18 +38,39 @@ class CalendarSubscriber implements EventSubscriberInterface
         $end = $calendar->getEnd();
         $filters = $calendar->getFilters();
 
-        // You may want to make a custom query from your database to fill the calendar
+        // Modify the query to fit to your entity and needs
+        // Change booking.beginAt by your start date property
+        $bookings = $this->bookingRepository
+            ->createQueryBuilder('booking')
+            ->where('booking.beginAt BETWEEN :start and :end OR booking.endAt BETWEEN :start and :end')
+            ->setParameter('start', $start->format('Y-m-d H:i:s'))
+            ->setParameter('end', $end->format('Y-m-d H:i:s'))
+            ->getQuery()
+            ->getResult();
 
-        $calendar->addEvent(new Event(
-            'Event 1',
-            new \DateTime('Tuesday this week'),
-            new \DateTime('Wednesdays this week')
-        ));
+         foreach ($bookings as $booking) {
+             // this create the events with your data (here booking data) to fill calendar
+             $bookingEvent = new Event(
+                 $booking->getTitle(),
+                 $booking->getBeginAt(),
+                 $booking->getEndAt() // If the end date is null or not defined, a all day event is created.
+             );
 
-        // If the end date is null or not defined, it creates a all day event
-        $calendar->addEvent(new Event(
-            'All day event',
-            new \DateTime('Friday this week')
-        ));
+             $bookingEvent->setOptions([
+                 'backgroundColor' => '#f7cf49',
+                 'borderColor' => '#f7cf49',
+                 'textColor' => 'black',
+             ]);
+             $bookingEvent->addOption(
+                 'url',
+                 $this->router->generate('booking_calendar', [
+                     'id' => $booking->getId(),
+                 ])
+             );
+    
+
+        //     // finally, add the event to the CalendarEvent to fill the calendar
+            $calendar->addEvent($bookingEvent);
+         }     
     }
 }
