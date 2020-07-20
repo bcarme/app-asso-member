81b5b966d3fe3f6c2726e84f372726fedb6e0fbe
bcarme <barbara_carme@yahoo.fr>
2020-07-20 11:12:40 +0200

    style on forms for booking and French date

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Booking;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,9 +14,20 @@ class BookingType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('beginAt')
-            ->add('endAt')
-            ->add('title')
+            ->add('beginAt', DateTimeType::class, [
+                'label' => 'Début',
+                'choice_translation_domain' => true,
+                'widget' => 'choice',
+                'years' => range(date('Y'), date('Y')+4),
+                ])
+            ->add('endAt', DateTimeType::class, [
+                'label' => 'Fin',
+                'choice_translation_domain' => true,
+                'widget' => 'choice',
+                'years' => range(date('Y'), date('Y')+4),
+                ])
+            ->add('title', TextType::class, [
+                'label' => 'Titre'])
         ;
     }
 
