9ff566c4d7863d4988238164ca6cda693c4a7e1a
bcarme <barbara_carme@yahoo.fr>
2020-07-21 12:27:32 +0200

    style of CRUD

--- src/Form/LocationType.php
+++ src/Form/LocationType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Location;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class LocationType extends AbstractType
@@ -12,10 +14,14 @@ class LocationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('place')
-            ->add('address')
-            ->add('zipcode')
-            ->add('city')
+            ->add('place', TextType::class, [
+                'label' => 'Lieu'])
+            ->add('address', TextType::class, [
+                'label' => 'Adresse'])
+            ->add('zipcode', NumberType::class, [
+                'label' => 'Code Postal'])
+            ->add('city', TextType::class, [
+                'label' => 'Ville'])
         ;
     }
 
