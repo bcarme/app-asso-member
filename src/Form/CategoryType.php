a7671d5eac237e96ff2278a3d507fe7f569d8e2c
bcarme <barbara_carme@yahoo.fr>
2020-07-21 14:56:45 +0200

    crud working and style done

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -6,14 +6,20 @@ use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('theme')
-            ->add('color')
+            ->add('theme', TextType::class, [
+                'label' => 'Titre',
+            ])
+            ->add('color', ColorType::class, [
+                'label' =>'Couleur'
+                ])        
         ;
     }
 
