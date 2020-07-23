ddfc491753c172e58b8226f86f4dc0ce5f6a5c94
Anthony Kwan Chung <anthonykwanchung@gmail.com>
2020-07-23 16:50:45 +0200

    When User registers for an event, User can only select Members he created

--- src/Form/BookingRegistrationType.php
+++ src/Form/BookingRegistrationType.php
@@ -4,7 +4,9 @@ namespace App\Form;
 
 use App\Entity\Member;
 use App\Entity\Registration;
+use App\Repository\MemberRepository;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,18 +15,31 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class BookingRegistrationType extends AbstractType
 {
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $user = $this->security->getUser();
         $builder
-        ->add('email', EmailType::class, [
-            'label' => 'Email',
-        ]) 
-        ->add('member', EntityType::class, [
-            'class' => Member::class,
-            'choice_label' => 'fullname',
-            'by_reference' => false,
-            'label' => 'Inscrire l\'adhérent',
-        ]);
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+            ])
+            ->add('member', EntityType::class, [
+                'class' => Member::class,
+                'query_builder' => function (MemberRepository $er) use ($user) {
+                    return $er->createQueryBuilder('u')
+                        ->where('u.user = :id')
+                        ->setParameter('id', $user)
+                        ->orderBy('u.firstname', 'ASC');
+                },
+                'choice_label' => 'fullname',
+                'label' => 'Inscrire l\'adhérent',
+                'by_reference' => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
