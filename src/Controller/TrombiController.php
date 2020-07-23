72129bf4aa51681eacbf8121d099eb3138e8250b
bcarme <barbara_carme@yahoo.fr>
2020-07-23 17:29:23 +0200

    and pagination added

--- src/Controller/TrombiController.php
+++ src/Controller/TrombiController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
 
 class TrombiController extends AbstractController
 {
@@ -16,7 +17,7 @@ class TrombiController extends AbstractController
      * @Route("/trombinoscope", name="app_trombi")
      * @return Response
      */
-    public function index(MemberRepository $memberRepository, Request $request):Response
+    public function index(MemberRepository $memberRepository, Request $request, PaginatorInterface $paginator):Response
     {
         $members = $this->getDoctrine()
         ->getRepository(Member::class)
@@ -30,6 +31,13 @@ class TrombiController extends AbstractController
         $search = $data['search'];
         $members =  $memberRepository->findByName($search);
     }
+
+
+    $members = $paginator->paginate(
+        $members, // Requête contenant les données à paginer (ici nos articles)
+        $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+        3// Nombre de résultats par page
+    );
     return $this->render('trombi/index.html.twig', [
         'members' => $members,
         'form' => $form->createView(),
