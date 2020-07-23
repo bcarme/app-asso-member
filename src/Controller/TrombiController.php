be1d2fe65f3669c432259e54917accce46ff324b
bcarme <barbara_carme@yahoo.fr>
2020-07-23 17:30:41 +0200

    change to 10 members per page

--- src/Controller/TrombiController.php
+++ src/Controller/TrombiController.php
@@ -36,7 +36,7 @@ class TrombiController extends AbstractController
     $members = $paginator->paginate(
         $members, // Requête contenant les données à paginer (ici nos articles)
         $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
-        3// Nombre de résultats par page
+        10// Nombre de résultats par page
     );
     return $this->render('trombi/index.html.twig', [
         'members' => $members,
